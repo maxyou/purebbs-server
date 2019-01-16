a95c2747633c42291c98d52c41b0e294eec5c7ba
maxyou <hyyou@foxmail.com>
2019-01-17 00:23:24 +0800

    session minimal mysql

--- middleware/session/index.js
+++ middleware/session/index.js
@@ -1,4 +1,6 @@
 const session = require("./session")
+const minimal = require("./minimal")
 module.exports = (app) => {
-   return session(app)
+   // return session(app)
+   return minimal(app)
 }
---
+++ middleware/session/minimal.js
@@ -0,0 +1,33 @@
+const session = require('koa-session-minimal')
+const MysqlSession = require('koa-mysql-session')
+
+// 配置存储session信息的mysql
+let store = new MysqlSession({
+    user: 'root',
+    password: '123456',
+    database: 'koa2session',
+    host: '127.0.0.1',
+})
+
+// 存放sessionId的cookie配置
+let cookie = {
+    maxAge: '', // cookie有效时长
+    expires: '',  // cookie失效时间
+    path: '', // 写cookie所在的路径
+    domain: '', // 写cookie所在的域名
+    httpOnly: '', // 是否只用于http请求中获取
+    overwrite: '',  // 是否允许重写
+    secure: '',
+    sameSite: '',
+    signed: '',
+
+}
+
+module.exports = () => {
+
+    return session({
+        key: 'SESSION_ID',
+        store: store,
+        cookie: cookie
+      })
+}
