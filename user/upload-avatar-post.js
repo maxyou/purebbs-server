5ce6e6be56a5a2928dd7f3fd15dcbc0250d06ee6
maxyou <hyyou@foxmail.com>
2019-02-15 01:54:26 +0800

    can upload

--- user/upload-avatar-post.js
+++ user/upload-avatar-post.js
@@ -1,10 +1,24 @@
-const User = require('./user')
+const formidable = require('formidable')
 
 module.exports = async (ctx, next) => {
 
-
-
+    let form = new formidable.IncomingForm()
     
+    form.uploadDir = `${__dirname}/`
+    form.keepExtensions = true
+
+    await new Promise((resolve, reject) => {
+        form.parse(
+            ctx.req,
+            (err, fields, files)=>{
+                if(err) return reject(err)
+                ctx.req.fields = fields
+                ctx.req.files = files
+                resolve()
+            }
+        )
+    })
+
     await ctx.render('upload-avatar-success', {
         title: 'Upload avatar success',
     })
