e02a443b918036f45237c89fdf5e538c198ef3fb
maxyou <hyyou2010@gmail.com>
2019-06-01 17:00:51 +0800

    login/out

--- src/controller/user/login-post.js
+++ src/controller/user/login-post.js
@@ -1,4 +1,4 @@
-const { user:service } = require('../../service')
+const { user: service } = require('../../service')
 
 module.exports = async (ctx, next) => {
   let {
@@ -7,32 +7,34 @@ module.exports = async (ctx, next) => {
     code
   } = ctx.request.body
 
-  
+
   let signin = { name, password, code }
-  
-  if(ctx.session && ctx.session.captchaText && code.toUpperCase()!=ctx.session.captchaText.toUpperCase()){
 
-    console.log('ctx.session.captchaText.toUpperCase not equ')
-    ctx.body = {code:-1, message:'验证码不符'}
-  
-  }else{//这里也许应该清除ctx.session.captchaText，防止客户端重复利用这个captchaText
+  if (ctx.session && ctx.session.captchaText && ctx.session.captchaText.toUpperCase() == code.toUpperCase()) {
 
-    console.log('sign-in/post:--------2' + name + ' ' + password + ' ' + code)    
+    console.log('sign-in/post:--------2' + name + ' ' + password + ' ' + code)
     var result = await service.authenticateUser(signin);
-  
+
     if (result && result.code == 0) {
       console.log('authen result:')
       console.log(result)
       ctx.session.userinfo = { isLogin: true, ...result.res._doc };
       console.log('session.userinfo:')
       console.log(ctx.session.userinfo)
-      ctx.body = {code:0, message:result.message, data:{//有选择地返回给客户端
-        _id:result.res._id, 
-        name:result.res.name
-      }}
-    }else{
-      ctx.body = {code:-1, message:result.message, data:{}}
-
+      ctx.body = {
+        code: 0, message: result.message, data: {//有选择地返回给客户端
+          _id: result.res._id,
+          name: result.res.name
+        }
+      }
+    } else {
+      ctx.body = { code: -1, message: result.message, data: {} }
     }
+
+  } else {//这里也许应该清除ctx.session.captchaText，防止客户端重复利用这个captchaText
+
+    console.log('ctx.session.captchaText.toUpperCase not equ')
+    ctx.body = { code: -1, message: '验证码不符' }
+
   }
 }
--- src/controller/user/register-post.js
+++ src/controller/user/register-post.js
@@ -1,45 +1,38 @@
-const {user:service} = require('../../service')
+const { user: service } = require('../../service')
 
 module.exports = async (ctx, next) => {
-    let {
-        name,
-        password,
-        code
-    } = ctx.request.body
-    console.log('sign-up/post:' + name + ' ' + password)
+  let {
+    name,
+    password,
+    code
+  } = ctx.request.body
+  console.log('sign-up/post:' + name + ' ' + password)
 
-    let signup = { name, password, code }
+  let signup = { name, password, code }
 
-    if(code.toUpperCase()!=ctx.session.captchaText.toUpperCase()){
-        console.log('ctx.session.captchaText.toUpperCase not equ')
-        return await ctx.render('public/message', {
-          title:'error',
-          message:'captchaText error',
-          delay:'3',
-          redirectUrl:'/login'
-        });    
-      }
+
+  if (ctx.session && ctx.session.captchaText && ctx.session.captchaText.toUpperCase() == code.toUpperCase()) {
 
     let result = await service.addUser(signup)
     console.log(result)
 
     if (result && result.code == 0) {
-        ctx.session.userinfo = { isLogin: true, ...result.res._doc };
-        console.log('ctx.session:')
-        console.log(ctx.session)
-        await ctx.render('user/register/submit', {
-          title: 'register success',
-          code: result.code,
-          message: result.message,
-        })
-      } else {
-        console.log('user is invalid')
-        await ctx.render('user/register/submit', {
-          title: 'register failed',
-          code: result.code,
-          message: result.message,
-        })
-      }
+      ctx.session.userinfo = { isLogin: true, ...result.res._doc };
+      console.log('ctx.session:')
+      console.log(ctx.session)
+      ctx.body = { code: 0, message: result.message, data: {} }
+    } else {
+      console.log('user is invalid')
+      ctx.body = { code: -1, message: result.message, data: {} }
+    }
+
     console.log('sign-up/post===============3')
-        
+
+  } else {//这里也许应该清除ctx.session.captchaText，防止客户端重复利用这个captchaText
+
+    console.log('ctx.session.captchaText.toUpperCase not equ')
+    ctx.body = { code: -1, message: 'captchaText error', data: {} }
+
+  }
+
 }
