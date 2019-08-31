d2724a1248ec0c543c257fb7cdc72ae40470ddc3
maxyou <hyyou2010@gmail.com>
2019-09-01 00:19:09 +0800

    can get github user data

--- src/service/sys.js
+++ src/service/sys.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
 const { time, calc } = require('../tool')
 const appConfig = require('../../config')
-
-
+console.log('before querystring require')
+const querystring = require('querystring');
+console.log('after querystring require')
 
 module.exports = {
 
@@ -15,19 +16,49 @@ module.exports = {
         console.log(appConfig.oauth_github.client_id)
         console.log(appConfig.oauth_github.client_secret)
 
-        var res = await axios.post('https://github.com/login/oauth/access_token', {
-            client_id: appConfig.oauth_github.client_id,
-            client_secret: appConfig.oauth_github.client_secret,
-            code:code,
-            // redirect_uri:'http://localhost:3000',
-            redirect_uri:'http://localhost:3001/oauth/redirect',
-            state:'123456'
-        })
+        // var res = await axios.post('https://github.com/login/oauth/access_token', {
+        //     client_id: appConfig.oauth_github.client_id,
+        //     client_secret: appConfig.oauth_github.client_secret,
+        //     code:code,
+        //     // redirect_uri:'http://localhost:3000',
+        //     redirect_uri:'http://localhost:3001/oauth/redirect',
+        //     state:'123456'
+        // })
+
+        var res = await axios({
+            method: 'post',
+            url: 'https://github.com/login/oauth/access_token?' +
+              `client_id=${appConfig.oauth_github.client_id}&` +
+              `client_secret=${appConfig.oauth_github.client_secret}&` +
+              `code=${code}`,
+            headers: {
+              accept: 'application/json'
+            }
+          });
+
+
         console.log('------- service oauthGithub ----------2')
-        console.log(res)
-        console.log('------- service oauthGithub ----------3')
+        // console.log(res)
+        console.log(res.data)
         console.log(res.data.access_token)
-        return { code: 0, message: '获取数据成功', data: {} };
+        console.log('------- service oauthGithub ----------3')
+        // let query = querystring.parse(res.data)
+        // console.log(query)
+        // console.log(JSON.stringify(query))
+
+
+        const result = await axios({
+            method: 'get',
+            url: `https://api.github.com/user`,
+            headers: {
+              accept: 'application/json',
+              Authorization: `token ${res.data.access_token}`
+            }
+          });
+
+        console.log('------- service oauthGithub ----------4')
+        console.log(result.data)
+        return { code: 0, message: '获取数据成功', data: {data:result.data} };
 
     },
     async categoryGet(query, ctx) {
